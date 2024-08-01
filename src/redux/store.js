a1f12810abd444bed14f26e94cd82046de6c7433
Sagar <deshpandesagar187@gmail.com>
2024-08-01 21:21:14 +0530

    Fixed error

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,6 @@ export const store = configureStore({
         [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
         player: playerReducer,
     },
-    middleware: (getDefaultMiddleware) => {
-        getDefaultMiddleware().concat(shazamCoreApi.middleware);
-    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
