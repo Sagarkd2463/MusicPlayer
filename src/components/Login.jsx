2d850e9d7cbae719a4baf37fd0bc30abcf0ec09a
Sagar <deshpandesagar187@gmail.com>
2024-10-22 19:26:51 +0530

    modified css of login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Login = () => {
 
     const handleClickSpotify = () => {
-        const clientID = import.meta.env.VITE_CLIENT_ID;
-        const redirectUrl = import.meta.env.VITE_REDIRECT_URL;
-        const apiUrl = import.meta.env.VITE_API_URL;
+        const clientId = import.meta.env.VITE_SPOTIFY_API_CLIENT_ID;
+        const redirectUrl = import.meta.env.VITE_SPOTIFY_REDIRECT_URL;
+        const apiUrl = import.meta.env.VITE_SPOTIFY_API_URL;
+
         const scope = [
             'user-read-email',
             'user-read-private',
@@ -17,17 +18,30 @@ const Login = () => {
             'user-read-recently-played'
         ];
 
-        window.location.href = `${apiUrl}?client_id=${clientID}&redirect_uri=${redirectUrl}&scope=${scope.join(
+        window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
             " "
         )}&response_type=token&show_dialog=true`;
     };
 
     return (
-        <div className='container'>
-            <img className='spotify-logo' src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Full_Logo_RGB_Green.png"
-                alt="spotify" />
-            <button onClick={handleClickSpotify} className='spotify-btn-connect'>Connect Spotify </button>
-        </div>
+        <>
+            <div className="relative flex items-center justify-center mt-16 mb-8 ml-16 mr-16">
+                <img
+                    className="w-auto h-12 md:h-16 lg:h-20 xl:h-28 2xl:h-32 object-cover"
+                    src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Full_Logo_RGB_Green.png"
+                    alt="spotify" />
+            </div>
+
+            <div className="relative flex items-center justify-center mt-16 ml-12 mr-5">
+                <button
+                    onClick={handleClickSpotify}
+                    className="text-center font-medium 
+                    pt-2 pb-2 pl-3 pr-3 bg-green-500 rounded-lg text-white shadow-lg 
+                    hover:bg-green-700 transition-all duration-300">
+                    Connect Spotify
+                </button>
+            </div>
+        </>
     );
 };
 
